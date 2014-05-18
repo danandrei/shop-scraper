9b05dfeac32027115c92a6da90d22446a7590a96
Dan Andrei <andrei_dani_96@yahoo.com>
2014-05-18 21:08:33 +0300

    added cron job

--- scrap.js
+++ scrap.js
@@ -4,16 +4,14 @@ var util = require('util'),
     mongo = require('./mongo_control.js'),
     ProgressBar = require('progress');
 var config = require('./config.json');
+var schedule = require('node-schedule');
 var db;
 var currentPosition = 0;
+var ACTIVE_DATE;
 
 // connect to mongo
 mongo.connect('scanner', function (mongoinfo) {
     db = mongoinfo;
-
-    // empty the db
-    // TODO this is only temporary until we figure out what to do with the old items when a new scan is called
-    mongo.dropDatabase(db);
 });
 
 // this function is used to move trough the hosts array
@@ -21,11 +19,11 @@ function nextHost () {
 
     // close app
     if (currentPosition == config.hosts.length - 1){
-        console.log('Scrapper will close in 10 seconds');
-        setTimeout(function() {
-            db.close();
-            process.exit();
-        }, 10000);
+        console.log('------ FINISHED RUN ------');
+        /* scrapper has finished  */
+
+        // TODO this is only temporary until we figure out what to do with the old items when a new scan is called
+        mongo.dropDatabase(db);
         return;
     }
 
@@ -180,5 +178,35 @@ function scrapHost (host) {
     });
 }
 
-// start the scraping
-scrapHost(config.hosts[0]);
+/****** CRON Job Core ******/
+
+function cronJob (oldDate) {
+    
+    // oldDate + 1 day
+    var date = new Date(oldDate.getTime() + (24 * 60 * 60 * 1000);
+
+    var job = schedule.scheduleJob(date, function(){
+        console.log('------ ' + date + ' ------');
+
+        // start the scrap
+        ACTIVE_DATE = date;
+        currentPosition = 0;
+        scrapHost(config.hosts[0]);
+
+        // call next cron job
+        cronJob(date);
+    });
+}
+
+// initialize the date
+var currentDate = new Date();
+var dateObj = {
+    year: currentDate.getUTCFullYear(),
+    month: currentDate.getUTCMonth(),
+    day: currentDate.getUTCDate()
+}
+var setHour = config.options.scrapHour.split(':');
+var scrapHour = new Date(dateObj.year, dateObj.month, dateObj.day, setHour[0], setHour[1], 0);
+
+// start the cron job
+cronJob(new Date(scrapHour - (24 * 60 * 60 * 1000)));
